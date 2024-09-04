50b213d0c62e2282d1eedb1bff4a0cef3c4fcd3e
viestamayriska <vristamayriska@gmail.com>
2024-09-04 10:54:28 +0700

    Praktikum2

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,32 @@
 <?php
 
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\PhotoController;
+
+
+
+// Praktikum 2
+Route::resource('photos', PhotoController::class);
+Route::resource('photos', PhotoController::class)->only([
+    'index', 'show'
+   ]);
+   Route::resource('photos', PhotoController::class)->except([
+    'create', 'store', 'update', 'destroy'
+   ]);
+   
+Route::get('/hello', [WelcomeController::class,'hello']);
+Route::get('/', [WelcomeController::class,'index']);
+Route::get('/about', [WelcomeController::class,'about']);
+Route::get('/articles/{Id}', [WelcomeController::class,'articles']);
+
+Route::get('/hello', [HomeController::class,'hello']);
+Route::get('/', [AboutController::class,'index']);
+Route::get('/about', [ArticleController::class,'about']);
+Route::get('/articles/{Id}', [WelcomeController::class,'articles']);
 
 /*
 |--------------------------------------------------------------------------
@@ -12,6 +38,8 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
+// Praktikum 1
 Route::get('/welcome', function () {
     return 'Selamat Datang Viesta';
 });
@@ -31,10 +59,10 @@ Route::get('/posts/{post}/comments/{comment}', function
     });
 
 
- Route::get('/articles/{id}', function
-    ($id) {
-     return 'Halaman Artikel dengan ID' .$id;
-    });
+// Route::get('/articles/{id}', function
+   // ($id) {
+     //return 'Halaman Artikel dengan ID' .$id;
+    //});
 
  Route::get('/user/{name?}', function ($name=null) {
         return 'Nama saya Rizka '.$name;
