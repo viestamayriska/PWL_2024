1b6178b573fb4e0cf6d833435c4269e8fb4e606d
viestamayriska <vristamayriska@gmail.com>
2024-09-04 09:18:39 +0700

    update praktikum1

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,29 @@ Route::get('/welcome', function () {
 Route::get('/NIM', function () {
  return '2241760134 Viesta';
 });
+
+Route::get('/user/{viesta}', function ($name) {
+    return 'Nama saya '.$name;
+    });
+
+Route::get('/posts/{post}/comments/{comment}', function
+    ($postId, $commentId) {
+     return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+    });
+
+
+ Route::get('/articles/{id}', function
+    ($id) {
+     return 'Halaman Artikel dengan ID' .$id;
+    });
+
+ Route::get('/user/{name?}', function ($name=null) {
+        return 'Nama saya Rizka '.$name;
+});
+
+
+Route::get('/user/{name?}', function ($name='John') {
+    return 'Nama saya '.$name;
+    });
+    
+    
